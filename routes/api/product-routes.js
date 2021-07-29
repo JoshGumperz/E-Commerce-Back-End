b162022966f1c2a8e37324d3f5e628bfa37eec1d
Josh Gumperz <joshg3023@gmail.com>
2021-07-28 23:04:49 -0700

    added code to category routes.js and product routes.js to handle responses to the /api/categories and /api/products endpoint

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,6 +5,14 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
@@ -12,6 +20,20 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
